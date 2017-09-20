2473733644c9b8f686a5a177f30096b9c558545c
kdreagle <kdreagle@gmail.com>
2017-09-20 16:04:45 -0400

    Cleaned up some more code and started task 3
    Added the climb_hill function which changes one value on the puzzle and updates the GUI with the new grid and solution.
    
    This task is tied to a new button on the bottom of the GUI

--- assignment1.py
+++ assignment1.py
@@ -3,7 +3,9 @@ from random import randint
 import collections
 
 
-class Puzzle: 
+class Puzzle:
+    grid = []
+    
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
@@ -25,7 +27,8 @@ class Puzzle:
         with open("C:/Users/Kyle/Documents/AI/asst1/test.txt") as f:
             lines = f.readlines()
             lines = [[int(i) for i in line.split()] for line in lines]
-        self.build_gui(frame,lines)
+        self.grid = lines
+        self.build_gui(frame)
 
 
     def generate_puzzle(self,frame):
@@ -36,31 +39,51 @@ class Puzzle:
                 max_jump = max(n-(x+1),x,n-(y+1),y)
                 random_jump = randint(1,max_jump)
                 row.append(random_jump)        
-            grid.append(row)
-        grid[n-1][n-1]=0
-        self.build_gui(frame,grid)
+            self.grid.append(row)
+        self.grid[n-1][n-1]=0
+        self.build_gui(frame)
 
-    def build_gui(self,frame,grid):
-        n = len(grid)
+    def build_graph(self):
+        n = len(self.grid)
+        graph = {}
+        # initializes the graph with no edges
         for x in range(0, n):
             for y in range(0,n):
-                self.button =  Button(frame,text=str(grid[x][y]),fg="red")
+                graph[(x+1,y+1)]=[]
+                
+        # finds the edges of the graph and adds them to the graph
+        for x in range(0, n):
+            for y in range(0,n):
+                # these if statements check which moves are possible
+                cell=self.grid[x][y]
+                if (x+cell <= n-1):
+                    graph[(x+1,y+1)].append((x+cell+1,y+1))
+                if (x-cell >= 0):
+                    graph[(x+1,y+1)].append((x-cell+1,y+1))
+                if (y+cell <= n-1):
+                    graph[(x+1,y+1)].append((x+1,y+cell+1))
+                if (y-cell >= 0):
+                    graph[(x+1,y+1)].append((x+1,y-cell+1))
+
+        # remove all edges going out from the goal cell back to itself
+        graph[(n,n)]=[]
+        return graph
+
+    def build_gui(self,frame):
+        n = len(self.grid)
+        for x in range(0, n):
+            for y in range(0,n):
+                self.button =  Button(frame,text=str(self.grid[x][y]),fg="red")
                 self.button.config(width=2)
                 self.button.grid(row=x+2,column=y)
                 
-        graph = build_graph(grid)
+        graph = self.build_graph()
         
         x_count = 0
-        solvable = False
         # create the gui showing the shortest distance to each cell
         for x in range(1, n+1):
             for y in range(1,n+1):
-                shortest_path=len(bfs(graph,(1,1),(x,y),n)) - 1 
-                if x == n and y == n and not shortest_path == -1 :
-                    label_str = "Function value: " + str(shortest_path)
-                    self.label = Label(frame, text=label_str, fg="black")
-                    self.label.grid(row=2*n+2, columnspan=n)
-                    solvable = True
+                shortest_path=len(bfs(graph,(1,1),(x,y),n)) - 1
                 if shortest_path == -1:
                     x_count += 1
                     self.button =  Button(frame,text="X",fg="blue")
@@ -71,38 +94,40 @@ class Puzzle:
                 self.button.grid(row=x+1+n,column=y-1)
                 
 
-        if not solvable:
+        if shortest_path == -1:
             x_count = x_count * -1
             label_str = "Function value: " + str(x_count)
             self.label = Label(frame, text=label_str, fg="black")
-            self.label.grid(row=2*n+2, columnspan=n)
+        else:
+            label_str = "Function value: " + str(shortest_path)
+            self.label = Label(frame, text=label_str, fg="black")
 
+        self.label.grid(row=2*n+2, columnspan=n)
 
+        self.climb_button = Button(frame,text="Climb Hill",fg="Purple", command=lambda: self.climb_hill(frame))
+        self.climb_button.grid(row=2*n+3, columnspan=n)
 
-def build_graph(grid):
-    n = len(grid)
-    graph = {}
-    # initializes the graph with no edges
-    for x in range(0, n):
-        for y in range(0,n):
-            graph[(x+1,y+1)]=[]
-            
-    # finds the edges of the graph and adds them to the graph
-    for x in range(0, n):
-        for y in range(0,n):
-            # these if statements check which moves are possible
-            if (x+grid[x][y] <= n-1):
-                graph[(x+1,y+1)].append((x+grid[x][y]+1,y+1))
-            if (x-grid[x][y] >= 0):
-                graph[(x+1,y+1)].append((x-grid[x][y]+1,y+1))
-            if (y+grid[x][y] <= n-1):
-                graph[(x+1,y+1)].append((x+1,y+grid[x][y]+1))
-            if (y-grid[x][y] >= 0):
-                graph[(x+1,y+1)].append((x+1,y-grid[x][y]+1))
+    # task 3 stuff
+    def climb_hill(self,frame):
+        n = len(self.grid)
+        
+        x,y = randint(1,n),randint(1,n)
+        
+        while x == n and y == n:
+            x,y = randint(1,n),randint(1,n)
+        
+        print(x,y)
+        
+        old_jump = self.grid[x-1][y-1]
+        max_jump = max(n-x,x-1,n-y,y-1)
+        new_jump = randint(1,max_jump)
+        while new_jump == old_jump:
+            new_jump = randint(1,max_jump)
 
-    # remove all edges going out from the goal cell back to itself
-    graph[(n,n)]=[]
-    return graph
+        self.grid[x-1][y-1] = new_jump
+        self.build_gui(frame)
+        
+
 
 def bfs(graph, start, goal, n):
     queue = []
@@ -123,6 +148,5 @@ def bfs(graph, start, goal, n):
 
 
 root = Tk()
-grid = []
 puzzle = Puzzle(root)
 root.mainloop()
