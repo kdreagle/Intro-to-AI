1784195135d6791b1e5b43131f5eb56c505abdfe
kdreagle <kdreagle@gmail.com>
2017-09-20 21:16:51 -0400

    Finished task 3
    Number of iterations per click is hard-coded on line 152. I think we need to have this as an input but I'll worry about that later.

--- assignment1.py
+++ assignment1.py
@@ -5,29 +5,28 @@ import collections
 
 class Puzzle:
     grid = []
+    bfs_grid = []
+    graph = {}
+    puzzle_values = []
+
     
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
 
-
-        #grid = read_puzzle()
-        #n = len(grid)
         self.generate_button = Button(frame,text="Generate Random Puzzle",fg="red", command=lambda: self.generate_puzzle(frame))
         self.read_button = Button(frame,text="Get Puzzle From File",fg="red", command=lambda: self.read_puzzle(frame))
         self.generate_button.grid(row=0,columnspan=10)
         self.read_button.grid(row=1,columnspan=10)
-        #self.enter_n(frame)
 
-    def enter_n(self,frame):
-        self.e = Entry(frame,text="Enter puzzle size")
-        self.e.grid(row=3,columnspan=n)
 
     def read_puzzle(self,frame):
         with open("C:/Users/Kyle/Documents/AI/asst1/test.txt") as f:
             lines = f.readlines()
             lines = [[int(i) for i in line.split()] for line in lines]
         self.grid = lines
+        self.build_graph()
+        self.bfs_the_whole_thing()
         self.build_gui(frame)
 
 
@@ -41,15 +40,18 @@ class Puzzle:
                 row.append(random_jump)        
             self.grid.append(row)
         self.grid[n-1][n-1]=0
+        self.build_graph()
+        self.bfs_the_whole_thing()
         self.build_gui(frame)
+
 
     def build_graph(self):
         n = len(self.grid)
-        graph = {}
-        # initializes the graph with no edges
+        self.graph={}
+        # initializes the graph with no edgesself.bfs_the_whole_thing()
         for x in range(0, n):
             for y in range(0,n):
-                graph[(x+1,y+1)]=[]
+                self.graph[(x+1,y+1)]=[]
                 
         # finds the edges of the graph and adds them to the graph
         for x in range(0, n):
@@ -57,58 +59,67 @@ class Puzzle:
                 # these if statements check which moves are possible
                 cell=self.grid[x][y]
                 if (x+cell <= n-1):
-                    graph[(x+1,y+1)].append((x+cell+1,y+1))
+                    self.graph[(x+1,y+1)].append((x+cell+1,y+1))
                 if (x-cell >= 0):
-                    graph[(x+1,y+1)].append((x-cell+1,y+1))
+                    self.graph[(x+1,y+1)].append((x-cell+1,y+1))
                 if (y+cell <= n-1):
-                    graph[(x+1,y+1)].append((x+1,y+cell+1))
+                    self.graph[(x+1,y+1)].append((x+1,y+cell+1))
                 if (y-cell >= 0):
-                    graph[(x+1,y+1)].append((x+1,y-cell+1))
+                    self.graph[(x+1,y+1)].append((x+1,y-cell+1))
 
         # remove all edges going out from the goal cell back to itself
-        graph[(n,n)]=[]
-        return graph
+        self.graph[(n,n)]=[]
 
+
+    def bfs_the_whole_thing(self):
+        self.bfs_grid = []
+        n = len(self.grid)
+        for x in range(1, n+1):
+            row = []
+            for y in range(1,n+1):
+                row.append(len(bfs(self.graph,(1,1),(x,y),n)) - 1)
+            self.bfs_grid.append(row)
+
+    
     def build_gui(self,frame):
         n = len(self.grid)
+        # create the gui of the puzzle
         for x in range(0, n):
             for y in range(0,n):
                 self.button =  Button(frame,text=str(self.grid[x][y]),fg="red")
                 self.button.config(width=2)
                 self.button.grid(row=x+2,column=y)
-                
-        graph = self.build_graph()
         
         x_count = 0
         # create the gui showing the shortest distance to each cell
-        for x in range(1, n+1):
-            for y in range(1,n+1):
-                shortest_path=len(bfs(graph,(1,1),(x,y),n)) - 1
+        for x in range(0, n):
+            for y in range(0,n):
+                shortest_path=self.bfs_grid[x][y]
                 if shortest_path == -1:
                     x_count += 1
                     self.button =  Button(frame,text="X",fg="blue")
                 else:
                     self.button =  Button(frame,text=str(shortest_path),fg="blue")
-
                 self.button.config(width=2)
-                self.button.grid(row=x+1+n,column=y-1)
+                self.button.grid(row=x+2+n,column=y)
                 
-
         if shortest_path == -1:
-            x_count = x_count * -1
-            label_str = "Function value: " + str(x_count)
-            self.label = Label(frame, text=label_str, fg="black")
+            function_value = x_count * -1
         else:
-            label_str = "Function value: " + str(shortest_path)
-            self.label = Label(frame, text=label_str, fg="black")
+            function_value = shortest_path
 
+        # show the function value in the gui
+        label_str = "Function value: " + str(function_value)
+        self.label = Label(frame, text=label_str, fg="black")
         self.label.grid(row=2*n+2, columnspan=n)
 
-        self.climb_button = Button(frame,text="Climb Hill",fg="Purple", command=lambda: self.climb_hill(frame))
+        # button to perform task 3
+        self.climb_button = Button(frame,text="Climb Hill",fg="Purple", command=lambda: self.climb_hill(frame,0))
         self.climb_button.grid(row=2*n+3, columnspan=n)
 
-    # task 3 stuff
-    def climb_hill(self,frame):
+
+    # task 3
+    def climb_hill(self,frame,iterations):
         n = len(self.grid)
         
         x,y = randint(1,n),randint(1,n)
@@ -116,18 +127,35 @@ class Puzzle:
         while x == n and y == n:
             x,y = randint(1,n),randint(1,n)
         
-        print(x,y)
-        
         old_jump = self.grid[x-1][y-1]
         max_jump = max(n-x,x-1,n-y,y-1)
         new_jump = randint(1,max_jump)
         while new_jump == old_jump:
             new_jump = randint(1,max_jump)
 
+        old_grid = self.grid
         self.grid[x-1][y-1] = new_jump
-        self.build_gui(frame)
         
+        old_graph = self.graph
+        self.build_graph()
 
+        old_bfs_grid = self.bfs_grid
+        self.bfs_the_whole_thing()    
+        
+        if self.bfs_grid[n-1][n-1] < old_bfs_grid[n-1][n-1]:
+            self.grid = old_grid
+            self.graph = old_graph
+            self.bfs_grid = old_bfs_grid
+        else:
+            self.puzzle_values.append(self.bfs_grid[n-1][n-1])
+
+        if iterations < 50:
+            self.climb_hill(frame,iterations+1)
+        else:
+            self.build_gui(frame)
+            print(self.puzzle_values)
+        
+        
 
 def bfs(graph, start, goal, n):
     queue = []
