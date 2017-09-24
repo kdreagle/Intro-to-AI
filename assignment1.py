cc48422daac3e7c0f3798e0a00889444ac903942
Yigit Gungor <yigitgungor@gmail.com>
2017-09-24 09:20:05 -0400

    Added task 6 body
    Task 6 button and body is in, needs to change the puzzle and get values assigned to the variables.

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import *
 import time
+import math
 
 
 class Puzzle:
@@ -9,18 +10,19 @@ class Puzzle:
     graph = {}
     start_time = 0
     p = 0
-    
+
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
 
-        self.generate_button = Button(frame,text="Generate Random Puzzle",fg="red", command=lambda: self.generate_puzzle(frame,int(input("Enter the size of the puzzle: "))))
-        self.read_button = Button(frame,text="Get Puzzle From File",fg="red", command=lambda: self.read_puzzle(frame))
-        self.generate_button.grid(row=0,columnspan=10)
-        self.read_button.grid(row=1,columnspan=10)
+        self.generate_button = Button(frame, text="Generate Random Puzzle", fg="red",
+                                      command=lambda: self.generate_puzzle(frame, int(
+                                          input("Enter the size of the puzzle: "))))
+        self.read_button = Button(frame, text="Get Puzzle From File", fg="red", command=lambda: self.read_puzzle(frame))
+        self.generate_button.grid(row=0, columnspan=10)
+        self.read_button.grid(row=1, columnspan=10)
 
-
-    def read_puzzle(self,frame):
+    def read_puzzle(self, frame):
         self.grid = []
         with open("C:/Users/Kyle/Documents/AI/asst1/test1.txt") as f:
             lines = f.readlines()
@@ -30,80 +32,77 @@ class Puzzle:
         self.bfs_the_whole_thing()
         self.build_gui(frame)
 
-
-    def generate_puzzle(self,frame,n):
+    def generate_puzzle(self, frame, n):
         self.grid = []
         for x in range(0, n):
             row = []
-            for y in range(0,n):
-                max_jump = max(n-(x+1),x,n-(y+1),y)
-                random_jump = randint(1,max_jump)
-                row.append(random_jump)        
+            for y in range(0, n):
+                max_jump = max(n - (x + 1), x, n - (y + 1), y)
+                random_jump = randint(1, max_jump)
+                row.append(random_jump)
             self.grid.append(row)
-        self.grid[n-1][n-1]=0
+        self.grid[n - 1][n - 1] = 0
         self.build_graph()
         self.bfs_the_whole_thing()
         if self.start_time == 0:
             self.build_gui(frame)
 
-
     def build_graph(self):
         n = len(self.grid)
-        self.graph={}
+        self.graph = {}
         # initializes the graph with no edgesself.bfs_the_whole_thing()
         for x in range(0, n):
-            for y in range(0,n):
-                self.graph[(x+1,y+1)]=[]
-                
+            for y in range(0, n):
+                self.graph[(x + 1, y + 1)] = []
+
         # finds the edges of the graph and adds them to the graph
         for x in range(0, n):
-            for y in range(0,n):
+            for y in range(0, n):
                 # these if statements check which moves are possible
-                cell=self.grid[x][y]
-                if (x+cell <= n-1):
-                    self.graph[(x+1,y+1)].append((x+cell+1,y+1))
-                if (x-cell >= 0):
-                    self.graph[(x+1,y+1)].append((x-cell+1,y+1))
-                if (y+cell <= n-1):
-                    self.graph[(x+1,y+1)].append((x+1,y+cell+1))
-                if (y-cell >= 0):
-                    self.graph[(x+1,y+1)].append((x+1,y-cell+1))
+                cell = self.grid[x][y]
+                if (x + cell <= n - 1):
+                    self.graph[(x + 1, y + 1)].append((x + cell + 1, y + 1))
+                if (x - cell >= 0):
+                    self.graph[(x + 1, y + 1)].append((x - cell + 1, y + 1))
+                if (y + cell <= n - 1):
+                    self.graph[(x + 1, y + 1)].append((x + 1, y + cell + 1))
+                if (y - cell >= 0):
+                    self.graph[(x + 1, y + 1)].append((x + 1, y - cell + 1))
 
         # remove all edges going out from the goal cell back to itself
-        self.graph[(n,n)]=[]
+        self.graph[(n, n)] = []
 
     def bfs_the_whole_thing(self):
         self.bfs_grid = []
         n = len(self.grid)
-        for x in range(1, n+1):
+        for x in range(1, n + 1):
             row = []
-            for y in range(1,n+1):
-                row.append(bfs(self.graph,(1,1),(x,y)))
+            for y in range(1, n + 1):
+                row.append(bfs(self.graph, (1, 1), (x, y)))
             self.bfs_grid.append(row)
 
-    
-    def build_gui(self,frame):
+    def build_gui(self, frame):
         n = len(self.grid)
         # create the gui of the puzzle
         for x in range(0, n):
-            for y in range(0,n):
-                self.button =  Button(frame,text=str(self.grid[x][y]),fg="red")
+            for y in range(0, n):
+                self.button = Button(frame, text=str(self.grid[x][y]), fg="red")
                 self.button.config(width=2)
-                self.button.grid(row=x+2,column=y)
-        
+                self.button.grid(row=x + 2, column=y)
+
         x_count = 0
         # create the gui showing the shortest distance to each cell
         for x in range(0, n):
-            for y in range(0,n):
-                shortest_path=self.bfs_grid[x][y]
+            for y in range(0, n):
+                shortest_path = self.bfs_grid[x][y]
                 if shortest_path == -1:
                     x_count += 1
-                    self.button =  Button(frame,text="X",fg="blue")
+                    self.button = Button(frame, text="X", fg="blue")
                 else:
-                    self.button =  Button(frame,text=str(shortest_path),fg="blue")
+                    self.button = Button(frame, text=str(shortest_path), fg="blue")
                 self.button.config(width=2)
-                self.button.grid(row=x+2+n,column=y)
-                
+                self.button.grid(row=x + 2 + n, column=y)
+
         if shortest_path == -1:
             function_value = x_count * -1
         else:
@@ -112,82 +111,116 @@ class Puzzle:
         # show the function value in the gui
         label_str = "Function value: " + str(function_value)
         self.label = Label(frame, text=label_str, fg="black")
-        self.label.grid(row=2*n+2, columnspan=n)
+        self.label.grid(row=2 * n + 2, columnspan=n)
 
         # button to perform task 3
-        self.climb_button = Button(frame,text="Climb Hill",fg="Purple", command=lambda: self.task4(frame,int(input("Enter how many iterations to climb: ")),0))
-        self.climb_button.grid(row=2*n+3, columnspan=n)
+        self.climb_button = Button(frame, text="Climb Hill", fg="Purple",
+                                   command=lambda: self.task4(frame, int(input("Enter how many iterations to climb: ")),
+                                                              0))
+        self.climb_button.grid(row=2 * n + 3, columnspan=n)
 
         # button to perform task 4
-        self.task4_button = Button(frame,text="Task 4",fg="Green", command=lambda: self.task4(frame,int(input("Enter how many iterations to climb: ")),int(input("Enter the number of random restarts: "))))
-        self.task4_button.grid(row=2*n+4, columnspan=n)
+        self.task4_button = Button(frame, text="Task 4", fg="Green",
+                                   command=lambda: self.task4(frame, int(input("Enter how many iterations to climb: ")),
+                                                              int(input("Enter the number of random restarts: "))))
+        self.task4_button.grid(row=2 * n + 4, columnspan=n)
 
         # button to perform task 5
-        self.task4_button = Button(frame,text="Task 5",fg="Brown", command=lambda: self.task5(frame,int(input("Enter how many iterations to climb: ")),float(input("Enter the probability of accepting a downhill move: "))))
-        self.task4_button.grid(row=2*n+5, columnspan=n)
+        self.task4_button = Button(frame, text="Task 5", fg="Brown",
+                                   command=lambda: self.task5(frame, int(input("Enter how many iterations to climb: ")),
+                                                              float(input(
+                                                                  "Enter the probability of accepting a downhill move: "))))
+        self.task4_button.grid(row=2 * n + 5, columnspan=n)
 
+        # button to perform task 6
+        self.task6_button = Button(frame, text="Task 6", fg="Brown",
+                                   command=lambda: self.task6(frame,
+                                                              int(input("Enter how many iterations: ")),
+                                                              float(input("Enter how initial temperature: ")),
+                                                              float(input("Enter decay rate: "))))
+        self.task6_button.grid(row=2 * n + 6, columnspan=n)
 
     # task 3  and task 4
-    def climb_hill(self,frame,iterations,puzzle_values):
+    def climb_hill(self, frame, iterations, puzzle_values):
 
         # start timer
         if self.start_time == 0:
             self.start_time = time.time()
-            
+
         n = len(self.grid)
-        
-        x,y = randint(1,n),randint(1,n)
-        
+
+        x, y = randint(1, n), randint(1, n)
+
         while x == n and y == n:
-            x,y = randint(1,n),randint(1,n)
-        
-        old_jump = self.grid[x-1][y-1]
-        max_jump = max(n-x,x-1,n-y,y-1)
-        new_jump = randint(1,max_jump)
+            x, y = randint(1, n), randint(1, n)
+
+        old_jump = self.grid[x - 1][y - 1]
+        max_jump = max(n - x, x - 1, n - y, y - 1)
+        new_jump = randint(1, max_jump)
         while new_jump == old_jump:
-            new_jump = randint(1,max_jump)
+            new_jump = randint(1, max_jump)
 
         old_grid = self.grid
-        self.grid[x-1][y-1] = new_jump
-        
+        self.grid[x - 1][y - 1] = new_jump
+
         old_graph = self.graph
         self.build_graph()
 
         old_bfs_grid = self.bfs_grid
         self.bfs_the_whole_thing()
 
-        
-        if self.bfs_grid[n-1][n-1] < old_bfs_grid[n-1][n-1] and random() > self.p:
+        if self.bfs_grid[n - 1][n - 1] < old_bfs_grid[n - 1][n - 1] and random() > self.p:
             self.grid = old_grid
             self.graph = old_graph
             self.bfs_grid = old_bfs_grid
         else:
-            puzzle_values.append(self.bfs_grid[n-1][n-1])
+            puzzle_values.append(self.bfs_grid[n - 1][n - 1])
 
         if iterations > 0:
-            self.climb_hill(frame,iterations-1,puzzle_values)
+            self.climb_hill(frame, iterations - 1, puzzle_values)
         else:
             total_time = time.time() - self.start_time
             print("Climbing the hill took " + str(total_time) + " seconds!")
             print(puzzle_values)
 
     # task 4 stuff
-    def task4(self,frame,iterations,restarts):
-        for i in range(0,restarts+1):
+    def task4(self, frame, iterations, restarts):
+        for i in range(0, restarts + 1):
             if (restarts > 0):
-                self.generate_puzzle(frame,len(self.grid))
-            self.climb_hill(frame,iterations,[])
-            
+                self.generate_puzzle(frame, len(self.grid))
+            self.climb_hill(frame, iterations, [])
+
         self.build_gui(frame)
 
-    def task5(self,frame,iterations,p):
+    def task5(self, frame, iterations, p):
         count = 0
         self.p = p
-        self.climb_hill(frame,iterations,[])
+        self.climb_hill(frame, iterations, [])
         self.p = 0
 
+    def task6(self, iterations, temp, decay):
+        for i in range(0, iterations):
 
-def bfs(graph,root,goal):
+            old_val = 0 #value of the old puzzle
+            new_val = 0 #value of the new puzzle
+
+            if new_val > old_val:
+                0
+                #grid = new_puzzle
+            else:
+                prob = math.exp((old_val-new_val)/temp)
+                if decision(prob):
+                    0
+                    #grid = new_puzzle
+
+            temp = temp * decay
+
+
+def decision(probability):
+    return random.random() <= probability
+
+
+def bfs(graph, root, goal):
     visited = []
     queue = [[root]]
     if root == goal:
@@ -205,7 +238,6 @@ def bfs(graph,root,goal):
                     return len(new_path) - 1
             visited.append(cell)
     return -1
-
 
 
 root = Tk()
